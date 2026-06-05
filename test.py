f2e32560495c92eef4eb6999fe5da782833d2397
l6eol <lidiwang@sjtu.edu.cn>
2026-06-05 11:17:59 +0800

    1

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-from retriever import (
-    hybrid_search,
-    save_retrieved_images,
-    search_by_caption,
-    search_by_image_embedding,
-)
+import os
+from config import DEFAULT_SAFETY_QUERY
+from rag_answer import build_image_rag_prompt
+from retriever import search_by_query_image
 
-query = "pieplines leak gas"
-results = search_by_image_embedding(query=query, top_k=3)
-
-print(results)
-
-save_retrieved_images(results)
+query_image = "/root/autodl-tmp/data/inspecsafe/DATA_PATH/test/Annotations/Normal_data/coal_conveyor-Level04-SuspendedRail-000001/coal_conveyor-Level04-SuspendedRail-000001-001.jpg"
+retrieved = search_by_query_image(query_image, top_k=3)
+prompt = build_image_rag_prompt(DEFAULT_SAFETY_QUERY, retrieved)
+print(prompt)
