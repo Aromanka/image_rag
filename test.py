ec11d341df31144640d2c82af109d7bed42814a5
l6eol <lidiwang@sjtu.edu.cn>
2026-06-06 16:14:04 +0800

    upd

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import os
 from config import DEFAULT_SAFETY_QUERY
 from rag_answer import build_image_rag_prompt
 from retriever import search_by_query_image
+from vlm_inference import VLM_inference, VLM_inference_with_RAG
+
 
 query_image = "/root/autodl-tmp/data/inspecsafe/DATA_PATH/test/Annotations/Normal_data/coal_conveyor-Level04-SuspendedRail-000001/coal_conveyor-Level04-SuspendedRail-000001-001.jpg"
-retrieved = search_by_query_image(query_image, top_k=3)
-prompt = build_image_rag_prompt(DEFAULT_SAFETY_QUERY, retrieved)
-print(prompt)
+result = VLM_inference_with_RAG('safety judgement', query_image, top_k=3)
+print(result['output'])
