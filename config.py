3a599625bfbfa602ececb09ac427e5c7922cb219
l6eol <lidiwang@sjtu.edu.cn>
2026-07-15 15:41:25 +0800

    1

--- config.py
+++ config.py
@@ -76,8 +76,8 @@ DEFAULT_CONSTRUCTIONSITE10K_QUERY = "Inspect this construction site image."
 DEFAULT_LAB_SAFETY_QUERY = "Answer the lab safety multiple-choice question."
 DEFAULT_LAB_SAFETY_GEN_QUERY = "Classify this laboratory scene as hazardous or non-hazardous."
 
-VLM_MODEL_PATH = "/root/autodl-tmp/model/qwenvl_2_5_3B"
-# VLM_MODEL_PATH = "/root/autodl-tmp/model/gemma3_4b"
+# VLM_MODEL_PATH = "/root/autodl-tmp/model/qwenvl_2_5_3B"
+VLM_MODEL_PATH = "/root/autodl-tmp/model/gemma3_4b"
 # VLM_MODEL_PATH = "/root/autodl-tmp/model/internvl2_4b"
 
 VLM_PROCESSOR_PATH = VLM_MODEL_PATH
